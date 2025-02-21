75008622ad6b5cc6ff52e1bbaebfb21a5598d86a
lee hyun woo <kaway0418@naver.com>
2025-02-21 23:32:38 +0900

    apprds add8

--- src/test/java/com/example/appRDS/AppRdsApplicationTests.java
+++ src/test/java/com/example/appRDS/AppRdsApplicationTests.java
@@ -12,21 +12,9 @@ import java.sql.ResultSet;
 @SpringBootTest
 class AppRdsApplicationTests {
 
-	@Autowired
-	private DataSource dataSource;
-
 	@Test
 	void contextLoads() {
 
-		try(Connection connection = dataSource.getConnection();
-			PreparedStatement preparedStatement = connection.prepareStatement("select now()");
-			ResultSet resultSet = preparedStatement.executeQuery();
-			){
-			resultSet.next();
-			System.out.println(resultSet.getString(1));
-		}catch (Exception e){
-			e.printStackTrace();
-		}
 	}
 
 }
